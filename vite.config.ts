9c85be374077139e8626bc5ea5556b942b4054e2
AntonVak <antonv.snab@gmail.com>
2023-09-26 11:34:04 +0300

    remove href="/vite.svg"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/vite-react-tailwind/",
   plugins: [react()],
-  base:"/vite-react-tailwind/"
+  
 })
